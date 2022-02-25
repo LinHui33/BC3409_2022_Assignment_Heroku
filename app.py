a2c04dc2e49afd1e86d29542a0d7b67951361fb9
LinHui33 <97448457+LinHui33@users.noreply.github.com>
2022-02-25 16:05:50 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,82 @@
+
+# coding: utf-8
+
+# In[20]:
+
+
+from flask import Flask
+
+
+# In[21]:
+
+
+app = Flask(__name__)
+
+
+# In[22]:
+
+
+from flask import request, render_template
+
+@app.route("/",methods = ["Get","POST"])
+def index():
+    if request.method == "POST":
+        income = float(request.form.get("income"))
+        age = float(request.form.get("age"))
+        loan = float(request.form.get("loan"))
+        
+        import joblib
+        import pandas as pd
+        import numpy as np
+        df = pd.read_csv("Credit Card Default II (balance).csv")
+        
+        #import different models
+        log_model = joblib.load("log_model")
+        cart_model = joblib.load("cart_model")
+        rf_model = joblib.load("rf_model")
+        xgb_model = joblib.load("XGB_model")
+        mlp_model = joblib.load("MLP_model")
+        
+        #Predicted values based on different model
+        log_pred = log_model.predict([[income,age,loan]])
+        cart_pred = cart_model.predict([[income,age,loan]])
+        rf_pred = rf_model.predict([[income,age,loan]])
+        xgb_pred = xgb_model.predict([[income,age,loan]])
+        
+        #Normalize the data for MLP model
+        norm_income = (income - df.income.min())/(df.income.max() - df.income.min())
+        norm_age = (age-df.age.min())/(df.age.max() - df.age.min())
+        norm_loan = (loan - df.loan.min())/(df.loan.max() - df.loan.min())
+
+        
+        log_pred = log_model.predict([[income,age,loan]])
+        cart_pred = cart_model.predict([[income,age,loan]])
+        rf_pred = rf_model.predict([[income,age,loan]])
+        xgb_pred = xgb_model.predict([[income,age,loan]])
+        mlp_pred = mlp_model.predict([[norm_income,norm_age,norm_loan]])
+
+        result1 = pd.DataFrame({'Model':['Log','CART','RF','XGB',"MLP"],
+                                'Outcome':[float(log_pred[0]),float(cart_pred[0]),
+                                           float(rf_pred[0]),float(xgb_pred[0]),float(mlp_pred[0])]})
+
+        result1['Default?'] = np.where(result1.Outcome>0.5,'Default', 'Not Default')
+        
+        s = f"""
+The predicted Outcome for various models are: 
+1. The outcome for Log Reg is {result1['Default?'][0]}
+2. The outcome for CART is {result1['Default?'][1]}
+3. The outcome for Random Forest is {result1['Default?'][2]}
+4. The outcome for XGBoost is {result1['Default?'][3]}
+5. The outcome for MLP is {result1['Default?'][4]}
+        """
+        return (render_template("index.html", result = s))
+    else:
+        return (render_template("index.html", result = "Please Enter the values"))
+
+
+# In[23]:
+
+
+if __name__ == "__main__":
+    app.run()
+
