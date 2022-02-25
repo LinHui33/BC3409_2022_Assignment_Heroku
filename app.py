3364ea12b4d1c114cbcf88dd07cf9e6b58e6387d
LinHui33 <97448457+LinHui33@users.noreply.github.com>
2022-02-25 16:34:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,43 +30,41 @@ def index():
 
         #import different models
         log_model = joblib.load("log_model")
-        cart_model = joblib.load("cart_model")
-        rf_model = joblib.load("rf_model")
-        xgb_model = joblib.load("XGB_model")
-        mlp_model = joblib.load("MLP_model")
+        #cart_model = joblib.load("cart_model")
+        #rf_model = joblib.load("rf_model")
+        #xgb_model = joblib.load("XGB_model")
+        #mlp_model = joblib.load("MLP_model")
         
         #Predicted values based on different model
         log_pred = log_model.predict([[income,age,loan]])
-        cart_pred = cart_model.predict([[income,age,loan]])
-        rf_pred = rf_model.predict([[income,age,loan]])
-        xgb_pred = xgb_model.predict([[income,age,loan]])
+        #cart_pred = cart_model.predict([[income,age,loan]])
+        #rf_pred = rf_model.predict([[income,age,loan]])
+        #xgb_pred = xgb_model.predict([[income,age,loan]])
         
         #Normalize the data for MLP model
-        norm_income = (income - 20014.48947)/(69995.685580 - 20014.48947)
-        norm_age = (age-18.055189)/(63.971796 - 18.055189)
-        norm_loan = (loan - 1.377630)/(13766.051240 - 1.377630)
+        #norm_income = (income - 20014.48947)/(69995.685580 - 20014.48947)
+        #norm_age = (age-18.055189)/(63.971796 - 18.055189)
+        #norm_loan = (loan - 1.377630)/(13766.051240 - 1.377630)
         
         #Preditions
         log_pred = log_model.predict([[income,age,loan]])
-        cart_pred = cart_model.predict([[income,age,loan]])
-        rf_pred = rf_model.predict([[income,age,loan]])
-        xgb_pred = xgb_model.predict([[income,age,loan]])
-        mlp_pred = mlp_model.predict([[norm_income,norm_age,norm_loan]])
+        #cart_pred = cart_model.predict([[income,age,loan]])
+        #rf_pred = rf_model.predict([[income,age,loan]])
+        #xgb_pred = xgb_model.predict([[income,age,loan]])
+        #mlp_pred = mlp_model.predict([[norm_income,norm_age,norm_loan]])
 
-        result1 = pd.DataFrame({'Model':['Log','CART','RF','XGB',"MLP"],
-                                'Outcome':[float(log_pred[0]),float(cart_pred[0]),
-                                           float(rf_pred[0]),float(xgb_pred[0]),float(mlp_pred[0])]})
+        #result1 = pd.DataFrame({'Model':['Log','CART','RF','XGB',"MLP"],'Outcome':[float(log_pred[0]),float(cart_pred[0]),float(rf_pred[0]),float(xgb_pred[0]),float(mlp_pred[0])]})
 
-        result1['Default?'] = np.where(result1.Outcome>0.5,'Default', 'Not Default')
+        #result1['Default?'] = np.where(result1.Outcome>0.5,'Default', 'Not Default')
         
-        s = f"""
-The predicted Outcome for various models are: 
-1. The outcome for Log Reg is {result1['Default?'][0]}
-2. The outcome for CART is {result1['Default?'][1]}
-3. The outcome for Random Forest is {result1['Default?'][2]}
-4. The outcome for XGBoost is {result1['Default?'][3]}
-5. The outcome for MLP is {result1['Default?'][4]}
-        """
+         s = f"""
+ The predicted Outcome for various models are: 
+ 1. The outcome for Log Reg is {result1['Default?'][0]}
+# 2. The outcome for CART is {result1['Default?'][1]}
+# 3. The outcome for Random Forest is {result1['Default?'][2]}
+# 4. The outcome for XGBoost is {result1['Default?'][3]}
+# 5. The outcome for MLP is {result1['Default?'][4]}
+         """
         return (render_template("index.html", result = s))
     else:
         return (render_template("index.html", result = "Please Enter the values"))
